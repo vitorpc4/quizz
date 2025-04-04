dae03d1dddbc055f79229ebec31d5a6c327ce10e
Fernando Amaral <feramal@gmail.com>
2025-04-03 21:46:37 -0300

    feat: Adiciona design da pergunta

--- src/app/quiz/[slug]/page.jsx
+++ src/app/quiz/[slug]/page.jsx
@@ -1,17 +1,42 @@
 "use client";
 
+import { Card, CardContent } from "@/Components/ui/card";
 import { use } from "react";
 
 export default function QuizSlugPage({ params }) {
   const paramsUrl = use(params);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] items-center sm:items-start">
-        <h1 className="text-2xl font-bold">Quiz Slug Page</h1>
-        <p className="text-lg">Welcome to the quiz slug page!</p>
-        <p className="text-lg">Slug: {paramsUrl.slug}</p>
-      </main>
+    <div className="space-y-6 w-[50rem] ">
+      <Card>
+        <CardContent className="w-full">
+          <p>Questão <span className="font-bold">1</span> de <span className="font-bold">10</span></p>
+          <p className="mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit?</p>
+          <form className="mt-4">
+            <label htmlFor="paris" className="block mt-3 p-4 border rounded-md cursor-pointer has-checked:ring-1 has-checked:ring-blue-400">
+              <input type="radio" id="paris" name="quiz" className="hidden" />
+              Paris
+            </label>
+
+            <label htmlFor="londres" className="block mt-3 p-4 border rounded-md cursor-pointer has-checked:ring-1 has-checked:ring-blue-400">
+              <input type="radio" id="londres" name="quiz" className="hidden" />
+              Londres
+            </label>
+
+            <label htmlFor="berlim" className="block mt-3 p-4 border rounded-md cursor-pointer has-checked:ring-1 has-checked:ring-blue-400">
+              <input type="radio" id="berlim" name="quiz" className="hidden" />
+              Berlim
+            </label>
+
+            <label htmlFor="madri" className="block mt-3 p-4 border rounded-md cursor-pointer has-checked:ring-1 has-checked:ring-blue-400">
+              <input type="radio" id="madri" name="quiz" className="hidden" />
+              Madri
+            </label>
+
+            <button type="submit" className="mt-8 w-full cursor-pointer bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-500">Próxima</button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
