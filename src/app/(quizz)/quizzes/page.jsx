8d79785ddf0721ffd73aad689adf44d571f4a219
Vitor <vitorhlaiam@gmail.com>
2025-04-13 11:29:57 -0300

    feat: Adiciona funcionalidade para bucar quizzes
    - Adiciona funcionalidade parabuscar quizzes de forma assíncrona e populara o estado com os dados recebidos do servidor.

--- src/app/(quizz)/quizzes/page.jsx
+++ src/app/(quizz)/quizzes/page.jsx
@@ -2,35 +2,40 @@
 
 import ListQuizzesComponent from "@/Components/Quiz/listQuizzes";
 import { Button } from "@/Components/ui/button";
+import instance from "@/http";
 import { useRouter } from "next/navigation";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Quizzes() {
   const router = useRouter();
 
-  const [quizzes, setQuizzes] = useState([
-    {
-      id: "2f09a884-52b7-431a-a43c-eaa440d80149",
-      name: "First Quiz",
-    },
-    {
-      id: "302730ff-df31-453f-b679-84d4f7d46736",
-      name: "Second Quiz",
-    },
-    {
-      id: "2e05e7e8-6595-4195-9b26-1f750e3981a5",
-      name: "Third Quiz",
-    },
-    {
-      id: "3cf37240-2a77-4214-a2e2-c9241e2bfb6e",
-      name: "Fourth Quiz",
-    },
-  ]);
+  const [quizzes, setQuizzes] = useState([]);
+
+  const getQuizzes = async () => {
+    const response = await instance.get("/quizzes");
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Erro ao buscar quizzes");
+    }
+  };
 
   const createQuiz = () => {
     router.push("/quiz");
   };
+
+  useEffect(() => {
+    const result = getQuizzes()
+      .then((res) => {
+        setQuizzes(res);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <div>
